a32b22e2daf05e5c49695bab25f20ee0e0e8ed79
JuanOssa1 <juan.ossa2@u.icesi.edu.co>
2022-09-22 20:17:14 -0500

    axios

---
+++ icesi-2022b-vue/src/components/DogComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card color="grey lighten-2">
+    <v-img src="https://goo.gl/6CQNDo"></v-img>
+    <v-card-title>
+      <div>
+        <h3>{{ dog.name }}</h3>
+        <p>{{ dog.breed }}</p>
+      </div>
+    </v-card-title>
+    <v-img :src="img" height="170px"></v-img>
+  </v-card>
+</template>
+<script>
+import axios from "../plugins/axios"
+
+export default ({
+    name: "DogComponent",
+    props: {
+        dog:{
+            type: Object,
+        },
+    },
+    data: function(){
+      return{
+        img:""
+      };
+    },
+     methods: {
+            getData: function(){
+                axios.get("/breed/"+ this.dog.breed + "/images/random").then(res =>{
+                    console.log(res);
+                    this.img = res.data.message
+                }).catch((error)=>{
+                    console.log(error);
+                })
+
+            }
+        },
+        created: function(){
+            this.getData();
+        }
+});
+
+</script>
--- icesi-2022b-vue/src/views/PetsView.vue
+++ icesi-2022b-vue/src/views/PetsView.vue
@@ -1,19 +1,28 @@
 <template>
     <v-container grid-list-md fluid>
-        <v-layout wram>
-            <v-flex xs12 sm4 md3>
-                <v-card color="grey lighten-2">
-                    <v-img src="https://goo.gl/6CQNDo"></v-img>
-                    <v-card-title>
-                        <div>
-                            <h3>Looking for a dog</h3>
-                        </div>
-                    </v-card-title>
-                </v-card>
+        <v-layout wrap>
+            <v-flex xs12 sm4 md3 v-for="dog in dogs" :key="dog.breed">
+                <appDog :dog="dog"/>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
+<script>
+    import {Dogs} from "../data/dogs"
+    import Dog from "../components/DogComponent.vue"
+    
+    export default {
+        components:{
+            appDog: Dog,
+        },
+        data: function(){
+            return{
+                dogs: Dogs,
+            };
+        },
+       
+    };
+</script>
 
 <style>
     p{
